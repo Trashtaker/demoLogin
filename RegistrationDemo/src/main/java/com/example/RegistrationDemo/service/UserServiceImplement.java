873b8820de97a657807343aa061500d87b959d63
Trashtaker <giangchu2233@gmail.com>
2024-05-13 07:37:09 +0700

    Add secur

--- RegistrationDemo/src/main/java/com/example/RegistrationDemo/service/UserServiceImplement.java
+++ RegistrationDemo/src/main/java/com/example/RegistrationDemo/service/UserServiceImplement.java
@@ -11,13 +11,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImplement implements UserService {
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
     private UserRepository userRepository;
 
     @Override
     public User save(UserDto userDto) {
-        User user = new User(userDto.getEmail(), userDto.getPassword() , userDto.getRole(), userDto.getFullname());
+        User user = new User(userDto.getEmail(), passwordEncoder.encode(userDto.getPassword()), userDto.getRole(), userDto.getFullname());
         return userRepository.save(user);
     }
 
